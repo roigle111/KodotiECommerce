d5bd468f869be355c851f119d6ee6c4bdfed8a62
roigle111 <rodrigoiledesma@gmail.com>
2022-04-18 19:15:27 -0300

    uklt

--- src/Services/Catalog/Catalog.Tests/ProductCreateEventHandlerTest.cs
+++ src/Services/Catalog/Catalog.Tests/ProductCreateEventHandlerTest.cs
@@ -1,7 +1,35 @@
-﻿namespace Catalog.Tests
+using Catalog.Domain;
+using Catalog.Service.EventHandlers;
+using Catalog.Service.EventHandlers.Commands;
+using Catalog.Tests.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading;
+
+namespace Catalog.Tests
 {
+    [TestClass]
     public class ProductCreateEventHandlerTest
     {
-        public ProductCreateEventHandlerTest()
+
+        [TestMethod]
+        public void TestCreateProduct()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+
+            var nameProduct = "Pantalon--55";
+
+            var handler = new ProductCreateEventHandler(context);
+            handler.Handle(new ProductCreateCommand {
+                Name = nameProduct, 
+                Description = "Descripcion del pantalon",
+                Price = 3600
+            }, new CancellationToken()).Wait();
+
+            var result = context.Products.Single(x => x.Name == nameProduct).Name;
+
+            Assert.AreEqual(nameProduct, result);
+        }
+
     }
 }
